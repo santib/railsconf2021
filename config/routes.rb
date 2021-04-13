6dd5fb0e1449d93de8924a46639c0f14aca19b35
Santiago Bartesaghi <santib@hey.com>
2021-04-13 20:19:01 -0300

    Root to references index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'references#index'
   resources :references
   get 'dynamic_forms/repeatable_fields/:id', to: 'dynamic_forms/repeatable_fields#new', as: :new_dynamic_forms_repeatable_field
   delete 'dynamic_forms/repeatable_fields/:id', to: 'dynamic_forms/repeatable_fields#delete', as: :delete_dynamic_forms_repeatable_field
