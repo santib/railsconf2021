c8832d1d8e6c4747d67bde47de8931f5edc11876
Santiago Bartesaghi <santib@hey.com>
2021-04-13 20:18:32 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+references_form = FormConfig.create!(title: 'references')
+references_form.field_configs.create!(
+  name: 'strengths_and_weaknesses',
+  label: "What are the applicant’s major strengths and weaknesses?",
+  field_type: :text_area,
+  options: nil,
+  position: 1,
+  required: true
+)
+references_form.field_configs.create!(
+  name: 'rating',
+  label: "How would you rate this applicant amongst his/her peers?",
+  field_type: :select,
+  options: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  position: 2,
+  required: false
+)
+references_form.field_configs.create!(
+  name: 'other_comments',
+  label: 'Other comments',
+  field_type: :text_area,
+  options: nil,
+  position: 3,
+  required: true,
+  repeatable: true
+)
